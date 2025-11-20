570a47d8d9f66c3c56e85665ae7393d3801ba190
elutiya <80179186+elutiya@users.noreply.github.com>
2025-11-20 07:57:21 -0500

    updating to do list app

--- todo_list_app.py
+++ todo_list_app.py
@@ -1,4 +1,3 @@
-# Enhanced To-Do List App (Numbered Tasks)
 # 3. To-Do List App (Text-Based)
 # Build a to-do list manager that:
 #  Allows users to add tasks with priorities (e.g., "Buy milk - high").
@@ -6,6 +5,7 @@
 #  Keeps looping until the user types “exit”.
 #  Shows a summary at the end: number of completed tasks vs pending.
 #  Skills practiced: lists, string parsing, loops, input, CRUD basics
+
 
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
