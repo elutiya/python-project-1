419b04f4830b054927615fbc13dcb4f6b59664bd
elutiya <80179186+elutiya@users.noreply.github.com>
2025-11-21 22:22:44 -0500

    updating simple grocery cart

--- Simple_Grocery_Cart_Checkout.py
+++ Simple_Grocery_Cart_Checkout.py
@@ -10,6 +10,8 @@ from colorama import Fore, Back, Style, init
 init(autoreset=True)
 import os
 import time
+import datetime
+import random
 #clear screen
 def clear_screen():
     os.system('cls')
@@ -36,18 +38,23 @@ BLUE = Fore.BLUE + Style.BRIGHT
 END = Style.RESET_ALL 
 HEADER_BG = "\033[48;2;1;140;136m" + Fore.WHITE + Style.BRIGHT
 OPTION_BG = "\033[48;2;147;184;183m" + Fore.BLACK + Style.NORMAL
+TITLE_BG= "\033[48;2;76;161;158m"+ Fore.WHITE + Style.NORMAL
 FOOTER_BG = "\033[48;2;1;140;136m" + Fore.WHITE + Style.BRIGHT
 
 total = 0
 #groceries_list
 groceries_lists =dict(onion = 5, tomato = 6, potato = 7, cabbage = 6, meat = 20 )
 cart={}
-print("Welcome to the Grocery Store!")
-print("Available items and prices:\n")
-print(f"{'Item':10} {'Price ($)':>13}")
-print("-" * 22)
+# print(HEADER_BG+ f"Welcome to the Grocery Store!")
+# print(HEADER_BG+ f"Available items and prices:\n")
+print(HEADER_BG + " " * 40)
+print(HEADER_BG + "        Available items and prices      ")
+print(HEADER_BG + " " * 40 + Style.RESET_ALL)
+    
+print(OPTION_BG+ f"{'Item':^15} {'Price ($)':^24}")
+print(OPTION_BG+ "-" * 40)
 for item, price in groceries_lists.items():
-    print(f"{item.title():13}  {price:.2f}")
+    print(OPTION_BG+f"{item.title():^15}  {price:^23.2f}")
 print()
 print("Type 'checkout' when you’re done")
 #accepting data from user
@@ -76,7 +83,15 @@ while True:
         cart[items] += qty
     else:
         cart[items] = qty
+now = datetime.datetime.now()
+date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+transaction_number = random.randint(10000, 99999)
 print("\n Reciept")
+print("abcd PLC")
+print("Vending Machine 001  Location: Arat Killo Plaza")
+print(f"Date & Time: {date_time}")
+print(f"Transaction #: {transaction_number}")
+print("-------------------------------------------------")
 print("-" * 30)
 for item, qty in cart.items():
     price = groceries_lists[item]   
